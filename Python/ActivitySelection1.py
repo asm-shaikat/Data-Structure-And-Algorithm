76112873d7bd4c757cdf3587d6d1e983f245b379
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2021-11-20 02:03:39 +0600

    ActivitySelection1 updated

--- Python/ActivitySelection1.py
+++ Python/ActivitySelection1.py
@@ -1,16 +1,14 @@
-n=int(input("How many activity:"))
-print("Enter start and finish time")
-
-temp=[]
-for i in range(n):
-    a,b=map(int,input("Activity %d:"%i).split())
-    temp.append([a,b])
-sortedAry=sorted(temp,key= lambda x:x[1])
-A=[0]
-i=0
-for z in range(1,n):
-    if(sortedAry[z
-    ][0]>=sortedAry[i][1]):
-        A.append(z)
-        i=z
-print(A)
+NoOfActivity = int(input("How many activity: "))
+print("Enter start time and finished time")
+Array = []
+selection=[0]
+for i in range(NoOfActivity):
+    x,y = map(int,input("Enter %d Activity: "%i).split())
+    Array.append([x,y])
+SortedArray = sorted(Array,key=lambda x:x[1])
+q=0
+for i in range(len(SortedArray)):
+    if(SortedArray[i][0] >= SortedArray[q][1]):
+        q=i
+        selection.append(i)
+print(selection)
