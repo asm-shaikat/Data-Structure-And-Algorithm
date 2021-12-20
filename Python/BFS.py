3fea6626d60e28249bd1013be0d418125f614a5e
Abu Shadat Shaikat <71976987+Shaikat1952@users.noreply.github.com>
2021-12-20 08:52:02 +0600

    Update BFS.py

--- Python/BFS.py
+++ Python/BFS.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import collections
 def bfs(graph, root):
 
@@ -22,29 +21,3 @@ def bfs(graph, root):
 if __name__ == '__main__':
     graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
     print("Following is Breadth First Traversal: ")
-=======
-import collections
-def bfs(graph, root):
-
-    visited, queue = set(), collections.deque([root])
-    visited.add(root)
-
-    while queue:
-
-        # Dequeue a vertex from queue
-        vertex = queue.popleft()
-        print(str(vertex) + " ", end="")
-
-        # If not visited, mark it as visited, and
-        # enqueue it
-        for neighbour in graph[vertex]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                queue.append(neighbour)
-
-
-if __name__ == '__main__':
-    graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
-    print("Following is Breadth First Traversal: ")
->>>>>>> 54a9d921fb7297289da1d50a4a430fbefcea0ba1
-    bfs(graph, 0)
